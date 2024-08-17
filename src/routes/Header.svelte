0cc6cf4d5ff6fc6b528ba7ae22005dc9ae977ba2
Michael <verenord@gmail.com>
2024-08-17 16:14:30 +0300

    fixed header z-index

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -3,7 +3,7 @@
     import DarkMode from './DarkMode.svelte';
 </script>
 
-<div class="sticky top-0 bg-base-200 navbar shadow-sm justify-between">
+<div class="sticky top-0 bg-base-200 navbar shadow-sm justify-between z-10">
     <h1 class="text-3xl font-bold px-4">Себестоимость</h1>
     <Theme/>
     <!-- <DarkMode/> -->
