8bfee94aa4c9c1c21b8b5378f5160f381b3e578d
Michael <verenord@gmail.com>
2024-08-17 15:57:08 +0300

    current version

--- src/routes/Table.svelte
+++ src/routes/Table.svelte
@@ -14,7 +14,13 @@
 		{ name: 'Масло', label: '💧 Масло' }
 	];
 
+	let examples2 = [
+		{ name: 'Доставка', label: '🚚 Доставка' },
+		{ name: 'Упаковка', label: '📦 Упаковка' }
+	];
+
 	let ingredients = $state([{ name: '', packageCost: '', packageVolume: '', recipeVolume: '' }]);
+	let consumables = $state([{ name: '', cost: '' }]);
 
 	//get ingredients from localStorage on launch
 	$effect(() => {
@@ -22,18 +28,29 @@
 		savedIngredients && (ingredients = JSON.parse(savedIngredients));
 	});
 
+	//get consumables from localStorage on launch
+	$effect(() => {
+		const savedConsumables = localStorage.getItem('consumables');
+		savedConsumables && (consumables = JSON.parse(savedConsumables));
+	});
+
 	//реактивное изменение ингредиентов
 	$effect(() => {
 		localStorage.setItem('ingredients', JSON.stringify(ingredients));
+	});
+
+	//реактивное изменение consumables
+	$effect(() => {
+		localStorage.setItem('consumables', JSON.stringify(consumables));
 	});
 
 	const deleteIngredient = (i: number) => {
 		ingredients = ingredients.filter((ingredient, index) => index !== i);
 	};
 
-
-
-
+	const deleteConsumable = (i: number) => {
+		consumables = consumables.filter((consumable, index) => index !== i);
+	};
 
 	const tableSum = $derived(() => {
 		return ingredients
@@ -95,7 +112,9 @@
 						<td><input type="number" bind:value={ingredient.recipeVolume} class="input input-bordered w-full max-w-xs" /></td>
 						<!-- кнопка удалить ингредиент -->
 						<td class="bg-base-200">
-							<button onclick={() => deleteIngredient(ingredients.indexOf(ingredient))}><X class="text-red-500" strokeWidth={3} size={20} /> </button>
+							<button onclick={() => deleteIngredient(ingredients.indexOf(ingredient))}
+								><X class="text-red-500" strokeWidth={3} size={20} />
+							</button>
 						</td>
 					</tr>
 				{/each}
@@ -124,6 +143,74 @@
 			Добавить ингредиент
 		</button>
 	</div>
+
+	<!-- render examples2 -->
+	<div class="flex flex-wrap gap-2 px-6 pt-8">
+		{#each examples2 as { name, label }}
+			<button class="btn btn-sm" onclick={() => consumables.push({ name, cost: '' })}>{label}</button>
+		{/each}
+	</div>
+
+	<div class="flex justify-start items-center px-6">
+		<h1 class="text-3xl font-bold">Расходники</h1>
+		<button
+			class="btn btn-xs ml-4"
+			onclick={() => {
+				consumables = [{ name: '', cost: '' }];
+			}}>
+			Очистить
+		</button>
+	</div>
+	<div class="border rounded-2xl overflow-hidden mx-2">
+		<table class="table">
+			<thead class="bg-base-200">
+				<tr>
+					<th scope="col"></th>
+					<th scope="col">📦 Расходник</th>
+					<th scope="col">Стоимость расхника <span class="font-normal italic">(₽)</span></th>
+					<th scope="col"></th>
+				</tr>
+			</thead>
+			<tbody>
+				{#each consumables as consumable (consumable)}
+					<tr in:fade>
+						<th class="text-xs bg-base-200 text-neutral w-fit">{consumables.indexOf(consumable) + 1}</th>
+						<th scope="row"><input type="text" bind:value={consumable.name} class="input input-bordered w-full" /></th>
+						<td><input type="number" bind:value={consumable.cost} class="input input-bordered w-full" /></td>
+						<td class="bg-base-200">
+							<button onclick={() => deleteConsumable(consumables.indexOf(consumable))}
+								><X class="text-red-500" strokeWidth={3} size={20} />
+							</button>
+						</td>
+					</tr>
+				{/each}
+			</tbody>
+			<tfoot>
+				<tr class="bg-base-200">
+					<td colspan="1"></td>
+					<!-- кнопка добавить новый ингредиент -->
+					<!-- <td colspan="1"><button class="btn btn-outline w-full border-dashed border-base-content/30" onclick={() => (ingredients = [...ingredients, { id: ingredients.length + 1, name: '', packageCost: '', packageVolume: '', recipeVolume: '' }])}> 
+					<Plus /> 
+				</button></td> -->
+					<!-- выводит сумму всех ингредиентов в рецепте -->
+					<td class="text-right text-red-500 text-lg" colspan="2">
+						Итого:
+						{tableSum()}&#8381;
+					</td>
+					<td colspan="1"></td>
+				</tr>
+			</tfoot>
+		</table>
+	</div>
+
+	<div class="flex justify-center gap-2 pb-8">
+		<button class="btn" onclick={() => consumables.push({ name: '', cost: '' })}> Добавить расходник </button>
+	</div>
+</div>
+
+
+<div class="flex justify-start items-center w-full rounded-2xl p-4 text-left bg-base-200">
+	<h1 class="text-3xl font-bold">Общий итог: </h1>
 </div>
 
 <style>
